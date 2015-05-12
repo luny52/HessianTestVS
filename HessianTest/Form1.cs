840def480a19be81d3910342c69c0fb812b036da
luny52 <luny52@163.com>
2015-05-12 15:58:27 +0800

    no 1

---
+++ HessianTest/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using hessiancsharp.client;
+
+
+namespace HessianTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = @"http://127.0.0.1:80/Hello";
+            CHessianProxyFactory factory = new CHessianProxyFactory();
+
+            Isay test = (Isay)factory.Create(typeof(Isay), url);
+
+            textBox1.Text = test.sayHello("C# call","C# p");
+            textBox1.Text += test.sayHello2("C# call", "C# p");
+        }
+    }
+}
---
+++ HessianTest/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HessianTest
+{
+    interface Isay
+    {
+         String sayHello(String arg1, String arg2);
+         String sayHello2(String arg1, String arg2);
+    }
+}
